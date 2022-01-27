31cde6f42efc617b1686464e05627bbadfeaceb0
Baktygul Kozhoshova <87691605+Baktygul-kk@users.noreply.github.com>
2022-01-27 21:29:03 +0600

    Задание 1

---
+++ script-1.js
@@ -0,0 +1,7 @@
+//1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере
+
+
+document.write(`<h2>Задача 1</h2>`);
+
+let message = "Hello, Kelechek!";
+document.write(message);
---
+++ script-3.js
@@ -0,0 +1,7 @@
+//3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+document.write(`<h2>Задача 3</h2>`);
+
+let name = "Baktygul",
+    email = "baktygul.kozhoshova.kk@gmail.com";
+
+    document.write(`<p>My name is ${name}</p> <span>My email is ${email}</span>`)
